f69a031b17543667c4620dd2e038fd4164ff6e76
Ngoc Vu The <ngocngoktvp10@gmail.com>
2019-05-28 14:27:32 +0700

    test change

--- app/models/User.php
+++ app/models/User.php
@@ -7,14 +7,4 @@ class User extends BaseModel
     protected $columns = [
         
     ];
-
-    public $colllll;
-    public $collll;
-    public $colll;
-    public $colll3;
-    public $coll;
-    public $coll7;
-    public $colllll4;
-    public $colll4;
-    
 }
